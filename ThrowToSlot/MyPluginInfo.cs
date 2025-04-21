63afa73b5cb3cae92661f7325a426a54b20724d3
Nagord <46509577+Nagord@users.noreply.github.com>
2025-04-21 16:41:14 -0700

    Updated documentation

--- ThrowToSlot/MyPluginInfo.cs
+++ ThrowToSlot/MyPluginInfo.cs
@@ -7,11 +7,11 @@ namespace ThrowToSlot
         public const string PLUGIN_GUID = "id107.throwtoslot";
         public const string PLUGIN_NAME = "ThrowToSlot";
         public const string USERS_PLUGIN_NAME = "Throw To Slot";
-        public const string PLUGIN_VERSION = "2.0.0";
+        public const string PLUGIN_VERSION = "2.0.1";
         public const string PLUGIN_DESCRIPTION = "If a thrown item hits an empty slot, it will be put in that slot.";
         public const string PLUGIN_ORIGINAL_AUTHOR = "18107";
         public const string PLUGIN_AUTHORS = "18107";
-        public const string PLUGIN_THUNDERSTORE_ID = "";
+        public const string PLUGIN_THUNDERSTORE_ID = "NihilityShift/Throw_To_Slot";
     }
 }
 #pragma warning restore CS1591
--- ThrowToSlot/ReleaseFiles/CHANGELOG.md
+++ ThrowToSlot/ReleaseFiles/CHANGELOG.md
@@ -1,3 +1,6 @@
+## 2.0.1
+- Updated for Void Crew 1.1.0
+
 ## 2.0.0
 - Changed to client side only
 - Now works on slots outside the ship
